0072446a4f207f7636c4f7552df480882f6c9b81
Pixel9113 <122548027+Pixel9113@users.noreply.github.com>
2023-05-03 11:01:50 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 input.onButtonPressed(Button.A, function () {
     comment.comment("DON'T CHANGE THIS CODE!")
+    speed = -200
     radio.sendValue("A", 1)
 })
 input.onButtonPressed(Button.AB, function () {
@@ -8,19 +9,22 @@ input.onButtonPressed(Button.AB, function () {
 })
 input.onButtonPressed(Button.B, function () {
     comment.comment("DON'T CHANGE THIS CODE!")
+    speed = -30
     radio.sendValue("B", 1)
 })
 let y = 0
 let x = 0
+let speed = 0
+speed = -30
 joystickbit.initJoystickBit()
 comment.comment("CHANGE THE RADIO GROUP TO YOUR TEAM'S JOYSTICK NUMBER!")
 comment.comment("DO NOT CHANGE ANYTHING ELSE IN THIS CODE")
-radio.setGroup(19)
+radio.setGroup(51)
 basic.showIcon(IconNames.Yes)
 basic.showString("T")
 basic.forever(function () {
     comment.comment("DON'T CHANGE THIS CODE!")
-    x = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.X), 1023, 0, -100, 100)
+    x = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.X), 1023, 0, speed, speed * -1)
     y = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.Y), 1023, 0, 100, -100)
     radio.sendValue("x", x)
     radio.sendValue("y", y)
